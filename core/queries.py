476c0adf0b666c9d68179a081cf0b3893ed4376e
ychamel <ychamel@hotmail.com>
2023-08-09 13:25:23 +0200

    fixes

--- core/queries.py
+++ core/queries.py
@@ -1,15 +1,20 @@
 import openai
-
+import streamlit as st
 from core.info import options
 
 
 class Chatbot:
-
     def __int__(self):
-        self.reset()
+        st.session_state.messages = [
+            {"role": "system",
+             "content": f"you are an excellent chatbot-navigator for a financing company that identifies which segment from this list {options} the user is looking for. You job is to identify which leaf node the user is looking for. \n"
+                        f"So for example the chosen segments can be one of the following: ['Automobile', 'consumer_finance', 'credit_limit'], but cannot be one of the following: ['Retail services','Auto & Trucks','Corporate & Medical centers']. \n"
+                        f"once you identify which segment the user want, write: '::stop::' followed by the segment name, This will navigate the user to the correct page with all information needed. \n"
+                        f"You don't need to elaborate about what each loan does you just needs to guide the user to identify which segment he's looking for to navigate him to the right spot."},
+        ]
 
     def reset(self):
-        self.messages = [
+        st.session_state.messages = [
             {"role": "system",
              "content": f"you are an excellent chatbot-navigator for a financing company that identifies which segment from this list {options} the user is looking for. You job is to identify which leaf node the user is looking for. \n"
                         f"So for example the chosen segments can be one of the following: ['Automobile', 'consumer_finance', 'credit_limit'], but cannot be one of the following: ['Retail services','Auto & Trucks','Corporate & Medical centers']. \n"
@@ -18,11 +23,10 @@ class Chatbot:
         ]
 
     def query(self, question):
-
-        self.messages.append({"role": "user", "content": question})
+        st.session_state.messages.append({"role": "user", "content": question})
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
-            messages=self.messages
+            messages=st.session_state.messages
         )
         answer = ""
         for choice in response.choices:
@@ -32,5 +36,5 @@ class Chatbot:
             self.reset()
             return "Stop"
 
-        self.messages.append({"role": "assistant", "content": answer})
+        st.session_state.messages.append({"role": "assistant", "content": answer})
         return answer
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import openai
 import streamlit as st
 from PIL import Image
 
@@ -8,7 +9,7 @@ from core.sidebar import sidebar
 st.set_page_config(page_title="Synapse Email Parser", page_icon="📖", layout="wide")
 
 # image
-image = Image.open('Main/assets/logo.png')
+image = Image.open('src/logo.png')
 st.image(image)
 # Title
 st.header("Synapse Email Parser")
@@ -17,26 +18,28 @@ st.header("Synapse Email Parser")
 sidebar()
 # open AI
 openai_api_key = st.session_state.get("OPENAI_API_KEY")
-
 if not openai_api_key:
     st.warning(
         "please enter a password to access the app!"
     )
+    st.stop()
+openai.api_key = openai_api_key
 
 # create class
-chatbot = st.session_state.get("CHATBOT")
-if not chatbot:
-    chatbot = Chatbot()
-    st.session_state["CHATBOT"] = chatbot
+chat_bot = st.session_state.get("CHATBOT")
+if not chat_bot:
+    chat_bot = Chatbot()
+    st.session_state["CHATBOT"] = chat_bot
 # create message log
 if not st.session_state.get("messages"):
     st.session_state["messages"] = []
 # chat
 question = st.text_input("Chat:")
-st.session_state.messages.append({"role": "user", "content": question})
-# query
-answer = chatbot.query(question)
-st.session_state.messages.append({"role": "assistant", "content": answer})
+if question:
+    st.session_state.messages.append({"role": "user", "content": question})
+    # query
+    answer = chat_bot.query(question)
+    st.session_state.messages.append({"role": "assistant", "content": answer})
 # display old chat
 for msg in reversed(st.session_state.messages):
     st.chat_message(msg["role"]).write(msg["content"])
